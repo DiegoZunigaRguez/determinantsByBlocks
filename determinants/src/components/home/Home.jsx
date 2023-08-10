19b788951db775de1e6bfb84580a97697a6d5a85
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2023-08-09 22:09:11 -0600

    Pantalla de inicio realizada

--- determinants/src/components/home/Home.jsx
+++ determinants/src/components/home/Home.jsx
@@ -5,11 +5,10 @@ import Data from './Data';
 
 function Home() {
   return (
-    <section className='home section' id='home'>
+    <section className='section' id='home'>
         <div className='home__container container grid'>
             <div className='home__content grid'>
                 <Social/>
-                <div className='home__img'></div>
                 <Data/>
             </div>
         </div>
