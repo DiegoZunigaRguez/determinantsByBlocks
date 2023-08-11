b70564f6b7692794b5b532173491792b54eb9cb1
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2023-08-11 13:28:26 -0600

    Se anexan los cambios responsive

--- determinants/src/App.js
+++ determinants/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Welcome from './components/welcome/welcome';
+import About from './components/about/about';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Home/>
       <Welcome/>
+      <About/>
     </>
   );
 }
---
+++ determinants/src/components/about/about.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.css";
+
+const about = () => {
+  return (
+    <section className="section about" id="about">
+      <div className="message">
+        <p>
+          A continuación, se presentará el modelo matemático con el cual esta
+          maravillosa aplicación funciona.
+        </p>
+        <br/><br/>
+        <p>De igual manera te dejamos el PDF para que puedas leer este trabajo:</p>
+      </div>
+    </section>
+  );
+};
+
+export default about;
