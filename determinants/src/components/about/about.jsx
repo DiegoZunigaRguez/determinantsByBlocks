c019cd3929ed05e86622bfbba6a486fff7f169aa
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2023-09-01 11:04:27 -0600

    Cambios en el header y en la pantalla de about

--- determinants/src/components/about/about.jsx
+++ determinants/src/components/about/about.jsx
@@ -7,8 +7,9 @@ const about = () => {
   return (
     <div className="about" id="about">
       <h1 className="about__title">Acerca del proyecto</h1>
-      <p className="about__text">Primer párrafo de contenido.</p>
-      <button>Botón</button>
+      <p className="about__advice">A continuación, se presentará el modelo matemático con el cual esta
+          maravillosa aplicación funciona. <br />De igual manera te dejamos el PDF para que puedas leer este trabajo: </p>
+      <button className="button button--flex__about"><a href={Document} download={''}>Descargar Documento</a></button>
       <p className="about__text">Segundo párrafo de contenido.</p>
     </div>
     /*<section className="section about" id="about">
--- determinants/src/components/header/Header.jsx
+++ determinants/src/components/header/Header.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header>
       <div className="back">
         <div className="menu contenedor">
           <a href="#home" className="logo">EscoMate</a>
-          <input type="checkbox" id="menu"></input>
+          <input type="checkbox" id="menu" checked={menuOpen} onChange={toggleMenu} />
           <label for="menu">
             <img src={require("../../assets/menu.png")} className="menu-icon" alt="" srcset="" />
           </label>
-          <nav className="navbar">
-            <ul>
-              <li><a href="#home">Inicio</a></li>
-              <li><a href="#about">Acerca del proyecto</a></li>
-              <li><a href="#determinants">Determinantes</a></li>
-              <li><a href="#simulation">Simulación</a></li>
-              <li><a href="#developers">Desarrolladores</a></li>
+          <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
+          <ul>
+              <li><a href="#home" onClick={closeMenu}>Inicio</a></li>
+              <li><a href="#about" onClick={closeMenu}>Acerca del proyecto</a></li>
+              <li><a href="#determinants" onClick={closeMenu}>Determinantes</a></li>
+              <li><a href="#simulation" onClick={closeMenu}>Simulación</a></li>
+              <li><a href="#developers" onClick={closeMenu}>Desarrolladores</a></li>
             </ul>
           </nav>
         </div>
